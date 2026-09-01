6f77ab881a4a65134d6c13b22939f7f047c8cab6
makikkk12 <dasheeyx01@gmail.com>
2026-09-01 19:04:01 +0800

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,45 @@
+var CACHE_NAME = 'typing-test-v1';
+var FILES_TO_CACHE = [
+  './',
+  './index.html',
+  './manifest.json',
+  './icon-192.png',
+  './icon-512.png'
+];
+
+self.addEventListener('install', function(event){
+  self.skipWaiting();
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(function(cache){
+      return cache.addAll(FILES_TO_CACHE);
+    })
+  );
+});
+
+self.addEventListener('activate', function(event){
+  event.waitUntil(
+    caches.keys().then(function(keys){
+      return Promise.all(
+        keys.filter(function(key){ return key !== CACHE_NAME; })
+            .map(function(key){ return caches.delete(key); })
+      );
+    })
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', function(event){
+  event.respondWith(
+    caches.match(event.request).then(function(cached){
+      if(cached) return cached;
+      return fetch(event.request).then(function(response){
+        return caches.open(CACHE_NAME).then(function(cache){
+          cache.put(event.request, response.clone());
+          return response;
+        });
+      }).catch(function(){
+        return caches.match('./index.html');
+      });
+    })
+  );
+});

44ad32c8b5d26008e532c991772a2fdd3de3d5b1
makikkk12 <dasheeyx01@gmail.com>
2026-09-01 19:30:10 +0800

    Update service worker cache version and fetch logic

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-var CACHE_NAME = 'typing-test-v1';
+var CACHE_NAME = 'typing-test-v2';
 var FILES_TO_CACHE = [
   './',
   './index.html',
@@ -29,14 +29,24 @@ self.addEventListener('activate', function(event){
 });
 
 self.addEventListener('fetch', function(event){
+  var url = event.request.url;
+
+  if(url.indexOf('/.netlify/functions/') !== -1){
+    event.respondWith(fetch(event.request));
+    return;
+  }
+
   event.respondWith(
     caches.match(event.request).then(function(cached){
       if(cached) return cached;
       return fetch(event.request).then(function(response){
-        return caches.open(CACHE_NAME).then(function(cache){
-          cache.put(event.request, response.clone());
-          return response;
-        });
+        if(response && response.ok){
+          return caches.open(CACHE_NAME).then(function(cache){
+            cache.put(event.request, response.clone());
+            return response;
+          });
+        }
+        return response;
       }).catch(function(){
         return caches.match('./index.html');
       });
